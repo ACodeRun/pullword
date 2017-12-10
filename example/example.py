66765178e0e7036ec24ba88ad8bdcc1997d44830
ACodeRun <hitercs@126.com>
2017-12-10 12:48:49 +0800

    pull

--- example/example.py
+++ example/example.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 # coding=utf-8
 from pullword import pullword
+import codecs
 
-print pullword(u"华中科技大学")
-print pullword(u"华中科技大学", debug=0)
-print pullword(u"华中科技大学", threshold=1)
+
+#pullword(u"华中科技大学")
+
+#print pullword(u"华中科技大学", debug=0)
+
+#print pullword(u"华中科技大学", threshold=1)
+
+
+f1 = open('temp.txt','w')
+f2 = codecs.open('example.data','r','utf-8')
+line = f2.readline()               
+while line:
+    word_list = pullword(line,threshold=0.8,debug=1)
+    print word_list,
+    f1.write(str(word_list)),
+    line = f2.readline()
+f1.close()
+f2.close()
